8069752cf4beb85aa42ba4405ad47781594f4a48
nettokrt <marcos.freitasnetto15@gmail.com>
2022-10-16 20:19:42 -0300

    Acesasndo site

---
+++ Tests/features/step_definitions/acessando_site.rb
@@ -0,0 +1,8 @@
+Quando('acesso a url') do
+  visit '/treinamento/home'
+  sleep(5)
+end
+  
+  Entao('eu verifico se estou na pagina correta') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
