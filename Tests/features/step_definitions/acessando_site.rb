e0a6ccbb95da717a0e410649d40ae45b20278063
nettokrt <marcos.freitasnetto15@gmail.com>
2022-10-16 21:25:54 -0300

    Trabalhando com validacoes

--- Tests/features/step_definitions/acessando_site.rb
+++ Tests/features/step_definitions/acessando_site.rb
@@ -1,8 +1,8 @@
 Quando('acesso a url') do
   visit '/treinamento/home'
   sleep(5)
+   
 end
-  
   Entao('eu verifico se estou na pagina correta') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+   expect(page). to have_current_path('https://automacaocombatista.herokuapp.com/treinamento/home')
+end
